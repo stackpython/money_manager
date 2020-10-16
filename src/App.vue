e55e29053ce9d4cb8c921b53a2ef7088129192fe
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-17 02:32:03 +0700

    more on vuex

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,54 @@
 <template>
 	<h2>Money Manager</h2>
-	<form @submit.prevent='onSubmit'>
+	<form @submit.prevent="onSubmit">
 		<div>
 			<label>บันทึก : </label>
-			<input v-model='note' type="text" placeholder="ข้าวไข่เจียว, ผงซักฟอก, อื่นๆ" />
+			<input
+				v-model="note"
+				type="text"
+				placeholder="ข้าวไข่เจียว, ผงซักฟอก, อื่นๆ"
+			/>
 		</div>
 
 		<div>
 			<label>ราคา: </label>
-			<input v-model='price' type="number" placeholder="100, 200" /> บาท
+			<input v-model="price" type="number" placeholder="100, 200" />
+			บาท
 		</div>
 
-		<button :disabled='buttonDisabled'>เพิ่มบันทึก</button>
+		<button :disabled="buttonDisabled">เพิ่มบันทึก</button>
 	</form>
+  <div v-for='(item, index) in $store.state.items' :key='index'>
+    {{ item.note }}
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  data() {
-    return {
-      note: '',
-      price: ''
-    }
-  },
-  computed: {
-    buttonDisabled() {
-      if (this.note === '' || this.price === '') {
-        return true;
-      }
-      return false;
-    }
-  },
-  methods: {
-    onSubmit() {
-      this.note = '';
-      this.price = '';
-    }
+	name: 'App',
+	data() {
+		return {
+			note: '',
+			price: '',
+		};
+	},
+	computed: {
+		buttonDisabled() {
+			if (this.note === '' || this.price === '') {
+				return true;
+			}
+			return false;
+		},
+	},
+	methods: {
+		onSubmit() {
+			this.$store.dispatch('addItem', {
+				note: this.note,
+				price: this.price,
+      });
+			this.note = '';
+			this.price = '';
+		},
   }
 };
 </script>
