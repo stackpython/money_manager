8f6d0b87f57e5c1add5f4045ad840bc37c9ad411
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2020-10-16 13:45:01 +0700

    vuex functions

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,17 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    items: []
   },
   mutations: {
+    ADD(state, item) {
+      state.items.push(item);
+    }
   },
   actions: {
+    addItem({ commit }, item) {
+      commit('ADD', item);
+    }
   },
   modules: {
   }
